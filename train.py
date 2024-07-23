e3393549a0912aa6f94d8f12985ccf30c04af404
hmhm715 <kmhm715@gmail.com>
2024-07-23 20:22:27 +0800

    Update train.py

--- train.py
+++ train.py
@@ -123,8 +123,7 @@ if __name__ == '__main__':
 
     # 数据集所在根目录
     # https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz
-    parser.add_argument('--data-path', type=str,
-                        default="/data/flower_photos")
+    parser.add_argument('--data-path', type=str, default="/data/flower_photos")
     parser.add_argument('--model-name', default='', help='create model name')
 
     # 预训练权重路径，如果不想载入就设置为空字符
